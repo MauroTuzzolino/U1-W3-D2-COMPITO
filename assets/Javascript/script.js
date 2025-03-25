88ca04fdaa29e98ad34fa27405fc84feb1982c05
Mauro Tuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-03-25 16:12:05 +0100

    completati esercizi: 1-14

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -97,7 +97,15 @@ paintItGreen();
         Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
        */
 
-const makeItClickable = function () {};
+const makeItClickable = function () {
+  const h1 = document.querySelector("h1");
+  let text = h1.innerText;
+  h1.addEventListener("click", function () {
+    if (text.length > 0) {
+      h1.innerText = text.slice(0, -1);
+    }
+  });
+};
 
 makeItClickable();
 
@@ -105,7 +113,13 @@ makeItClickable();
         Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
        */
 
-const revealFooterLink = function () {};
+const revealFooterLink = function () {
+  const footer = document.querySelector("footer");
+  const content = footer.innerText;
+  footer.addEventListener("click", function () {
+    alert(content);
+  });
+};
 
 revealFooterLink();
 
@@ -114,7 +128,17 @@ revealFooterLink();
         La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
      */
 
-const generateTable = function () {};
+const generateTable = function () {
+  const tableArea = document.getElementById("tableArea");
+  const table = document.createElement("table");
+  table.style.width = "100%";
+  table.setAttribute("border", "1");
+  let header = table.createTHead();
+  let headerRow = header.insertRow(0);
+  headerRow.innerHTML = "<th>Immagine</th><th>Nome Prodotto</th><th>Quantità</th><th>Prezzo</th>";
+  let tbody = table.createTBody();
+  tableArea.appendChild(table);
+};
 
 generateTable();
 
@@ -122,13 +146,32 @@ generateTable();
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
      */
 
-const addRow = function () {};
+const addRow = function (img, nome, quantita, prezzo) {
+  const table = document.querySelector("table");
+  let tBody = document.getElementsByTagName("tbody")[0];
+
+  let row = tBody.insertRow();
+  row.insertCell(0).innerHTML = "<img src='' alt='Immagine prodotto'>";
+  row.insertCell(1).innerText = nome;
+  row.insertCell(2).innerText = quantita;
+  row.insertCell(3).innerText = prezzo;
+};
+
+addRow("img", "latte", "5 litri", "50 euro");
 
 /* ESERCIZIO 14
        Crea una funzione che nasconda le immagini della tabella quando eseguita
      */
 
-const hideAllImages = function () {};
+const hideAllImages = function () {
+  const table = document.querySelector("table");
+  const images = document.getElementsByTagName("img");
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+};
+
+hideAllImages();
 
 /* EXTRA ESERCIZIO 15
        Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
